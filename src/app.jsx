2b5359014ec286f852382334e568475a0578d937
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2015-12-09 17:21:41 +0700

    Add style file

--- src/app.jsx
+++ src/app.jsx
@@ -5,9 +5,11 @@ var ReactDOM = require('react-dom');
 
 var App = React.createClass({
   render: function(){
-    return <div>
-      <h1>Hello, world</h1>
-    </div>
+    return (
+      <div>
+        <h1>Hello, world</h1>
+      </div>
+    );
   }
 });
 
