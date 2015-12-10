f1f80b5b9c04faa7bc52ac6e4df86fc9634bdb69
Arnupharp Viratanapanu <topscores@Arnupharps-MacBook-Pro.local>
2015-12-11 01:07:14 +0700

    Refractor navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 Navbar = React.createClass({
   render: function() {
     return (
-      <nav className="navbar navbar-default navbar-fixtop navbar-transparent">
+      <nav className="navbar navbar-default navbar-transparent">
         <div className="container">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu-collapse" aria-expanded="false">
