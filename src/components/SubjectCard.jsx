b9639a804db5c9b3ebb62828aeb4bd680d1a9cbf
Arnupharp Viratanapanu <topscores@Arnupharps-MacBook-Pro.local>
2015-12-20 01:47:33 +0700

    Fix bug

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -3,7 +3,7 @@ var React = require('React');
 var SubjectCard = React.createClass({
   render: function() {
     return (
-      <div className="col-lg-4 col-lg-4 col-sm-6 col-xs-1">
+      <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12">
         <div className="subject-card">
           <img src={this.props.img} />
           <div className="subject-name">{this.props.name}</div>
